db65b38b7937179c8cbf93e9da41f410dbabb6c7
BlackGoku36 <urjasvisuthar@gmail.com>
2023-02-18 15:27:46 +0530

    A triangle !! ! !

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,48 @@
 const std = @import("std");
 const mach = @import("mach");
+const gpu = @import("gpu");
 
 pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 core: mach.Core,
+pipeline: *gpu.RenderPipeline,
+queue: *gpu.Queue,
 
 pub fn init(app: *App) !void {
     try app.core.init(gpa.allocator(), .{});
+    app.core.setTitle("yohohoho");
+
+    const shader_module = app.core.device().createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
+
+    const blend = gpu.BlendState{};
+    const color_target = gpu.ColorTargetState{
+        .format = app.core.descriptor().format,
+        .blend = &blend,
+        .write_mask = gpu.ColorWriteMaskFlags.all
+    };
+
+    const fragment = gpu.FragmentState.init(.{
+        .module = shader_module,
+        .entry_point = "frag_main",
+        .targets = &.{color_target}
+    });
+
+    const vertex = gpu.VertexState.init(.{
+        .module = shader_module,
+        .entry_point = "vert_main",
+    });
+
+    const pipeline_desc = gpu.RenderPipeline.Descriptor{
+        .fragment = &fragment,
+        .vertex = vertex,
+    };
+
+    app.pipeline = app.core.device().createRenderPipeline(&pipeline_desc);
+    app.queue = app.core.device().getQueue();
+
+    shader_module.release();
 }
 
 pub fn deinit(app: *App) void {
@@ -24,5 +58,30 @@ pub fn update(app: *App) !bool {
             else => {},
         }
     }
+
+    const back_buffer_view = app.core.swapChain().getCurrentTextureView();
+    const color_attachment = gpu.RenderPassColorAttachment{
+        .view = back_buffer_view,
+        .clear_value = std.mem.zeroes(gpu.Color),
+        .load_op = .clear,
+        .store_op = .store,
+    };
+    const command_encoder = app.core.device().createCommandEncoder(null);
+    const render_pass_info = gpu.RenderPassDescriptor.init(.{
+        .color_attachments = &.{color_attachment},
+    });
+    const pass = command_encoder.beginRenderPass(&render_pass_info);
+    pass.setPipeline(app.pipeline);
+    pass.draw(3, 1, 0, 0);
+    pass.end();
+    pass.release();
+
+    var command = command_encoder.finish(null);
+    command_encoder.release();
+    app.queue.submit(&[_]*gpu.CommandBuffer{command});
+    command.release();
+    app.core.swapChain().present();
+    back_buffer_view.release();
+
     return false;
 }
