26d4f835bb2cb5aa9606859ceb630a2acbaa788c
BlackGoku36 <urjasvisuthar@gmail.com>
2023-03-10 21:14:10 +0530

    Put vertices data in CPU side

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,21 @@ pub const App = @This();
 
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
+pub const Vertex = extern struct {
+    pos: @Vector(4, f32),
+    col: @Vector(4, f32),
+};
+
+const vertices = [_]Vertex{
+    .{.pos = .{ 0.0, 0.5, 0.1, 1.0 }, .col = .{1.0, 1.0, 0.0, 1.0}},
+    .{.pos = .{ -0.5, -0.5, 0.1, 1.0 }, .col = .{1.0, 1.0, 0.0, 1.0}},
+    .{.pos = .{ 0.0, -0.5, 0.1, 1.0 }, .col = .{1.0, 1.0, 0.0, 1.0}},
+};
+
 core: mach.Core,
 pipeline: *gpu.RenderPipeline,
 queue: *gpu.Queue,
+vertex_buffer: *gpu.Buffer,
 
 pub fn init(app: *App) !void {
     try app.core.init(gpa.allocator(), .{});
@@ -17,22 +29,39 @@ pub fn init(app: *App) !void {
     const shader_module = app.core.device().createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
 
     const blend = gpu.BlendState{};
-    const color_target = gpu.ColorTargetState{
-        .format = app.core.descriptor().format,
-        .blend = &blend,
-        .write_mask = gpu.ColorWriteMaskFlags.all
+    const color_target = gpu.ColorTargetState{ .format = app.core.descriptor().format, .blend = &blend, .write_mask = gpu.ColorWriteMaskFlags.all };
+
+    const fragment = gpu.FragmentState.init(.{ .module = shader_module, .entry_point = "frag_main", .targets = &.{color_target} });
+
+    const vertex_attributes = [_]gpu.VertexAttribute{
+        .{.format = .float32x4, .offset = @offsetOf(Vertex, "pos"), .shader_location = 0},
+        .{.format = .float32x4, .offset = @offsetOf(Vertex, "col"), .shader_location = 1},
     };
 
-    const fragment = gpu.FragmentState.init(.{
-        .module = shader_module,
-        .entry_point = "frag_main",
-        .targets = &.{color_target}
+    const vertex_buffer_layout = gpu.VertexBufferLayout.init(.{
+        .array_stride = @sizeOf(Vertex),
+        .step_mode = .vertex,
+        .attributes = &vertex_attributes,
     });
+
+    const vertex_buffer = app.core.device().createBuffer(&.{
+        .usage = .{.vertex = true},
+        .size = @sizeOf(Vertex) * vertices.len,
+        .mapped_at_creation = true,
+    });
+
+    var vertex_mapped = vertex_buffer.getMappedRange(Vertex, 0, vertices.len);
+    std.mem.copy(Vertex, vertex_mapped.?, vertices[0..]);
+    vertex_buffer.unmap();
+
+    app.vertex_buffer = vertex_buffer;
 
     const vertex = gpu.VertexState.init(.{
         .module = shader_module,
         .entry_point = "vert_main",
+        .buffers = &.{vertex_buffer_layout},
     });
+
 
     const pipeline_desc = gpu.RenderPipeline.Descriptor{
         .fragment = &fragment,
@@ -72,7 +101,8 @@ pub fn update(app: *App) !bool {
     });
     const pass = command_encoder.beginRenderPass(&render_pass_info);
     pass.setPipeline(app.pipeline);
-    pass.draw(3, 1, 0, 0);
+    pass.setVertexBuffer(0, app.vertex_buffer, 0, @sizeOf(Vertex) * vertices.len);
+    pass.draw(vertices.len, 1, 0, 0);
     pass.end();
     pass.release();
 
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,12 +1,17 @@
-@vertex fn vert_main(@builtin(vertex_index) VertexIndex : u32 ) -> @builtin(position) vec4<f32> {
-    var pos = array<vec2<f32>, 3>(
-        vec2<f32>( 0.0,  0.5),
-        vec2<f32>(-0.5, -0.5),
-        vec2<f32>( 0.5, -0.5)
-    );
-    return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+struct VertexOut{
+    @builtin(position) position: vec4<f32>,
+    @location(0) color: vec4<f32>,
 }
 
-@fragment fn frag_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+@vertex 
+fn vert_main(@location(0) position : vec4<f32>, @location(1) color: vec4<f32>) -> VertexOut{
+    var output: VertexOut;
+    output.position = position;
+    output.color = color;
+    return output;
+}
+
+@fragment 
+fn frag_main(@location(0) color: vec4<f32>) -> @location(0) vec4<f32> {
+    return color;
 }
