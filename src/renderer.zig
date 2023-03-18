04fbde86297a6ef0d8763b0207888c2193283afd
BlackGoku36 <urjasvisuthar@gmail.com>
2023-03-18 17:21:48 +0530

    Move main renderer to it's own file

---
+++ src/renderer.zig
@@ -0,0 +1,120 @@
+const std = @import("std");
+const mach = @import("mach");
+const gpu = @import("gpu");
+
+pub const Vertex = extern struct {
+    pos: @Vector(4, f32),
+    col: @Vector(4, f32),
+};
+
+pub const Renderer = struct {
+
+    core: *mach.Core,
+    pipeline: *gpu.RenderPipeline,
+    queue: *gpu.Queue,
+    vertex_buffer: *gpu.Buffer,
+    vertices: std.ArrayList(Vertex),
+
+    pub fn init(allocator: std.mem.Allocator, core: *mach.Core) Renderer {
+        var vertices = std.ArrayList(Vertex).init(allocator);
+        const shader_module = core.device().createShaderModuleWGSL("shader.wgsl", @embedFile("shader.wgsl"));
+
+        const blend = gpu.BlendState{};
+        const color_target = gpu.ColorTargetState{ .format = core.descriptor().format, .blend = &blend, .write_mask = gpu.ColorWriteMaskFlags.all };
+
+        const fragment = gpu.FragmentState.init(.{ .module = shader_module, .entry_point = "frag_main", .targets = &.{color_target} });
+
+        const vertex_attributes = [_]gpu.VertexAttribute{
+            .{.format = .float32x4, .offset = @offsetOf(Vertex, "pos"), .shader_location = 0},
+            .{.format = .float32x4, .offset = @offsetOf(Vertex, "col"), .shader_location = 1},
+        };
+
+        const vertex_buffer_layout = gpu.VertexBufferLayout.init(.{
+            .array_stride = @sizeOf(Vertex),
+            .step_mode = .vertex,
+            .attributes = &vertex_attributes,
+        });
+
+        const vertex_buffer = core.device().createBuffer(&.{
+            .usage = .{.vertex = true, .copy_dst = true},
+            .size = @sizeOf(Vertex) * 6 * 10,
+        });
+
+        const vertex = gpu.VertexState.init(.{
+            .module = shader_module,
+            .entry_point = "vert_main",
+            .buffers = &.{vertex_buffer_layout},
+        });
+
+        const pipeline_desc = gpu.RenderPipeline.Descriptor{
+            .fragment = &fragment,
+            .vertex = vertex,
+        };
+
+        var pipeline = core.device().createRenderPipeline(&pipeline_desc);
+        var queue = core.device().getQueue();
+
+        shader_module.release();
+
+        return Renderer {
+            .core = core,
+            .pipeline = pipeline,
+            .queue = queue,
+            .vertex_buffer = vertex_buffer,
+            .vertices = vertices,
+        };
+    }
+
+    pub fn deinit(renderer: *Renderer) void {
+        renderer.vertices.deinit();
+    }
+
+    pub fn render(renderer: *Renderer) !void{
+        renderer.queue.writeBuffer(renderer.vertex_buffer, 0, renderer.vertices.items[0..]);
+
+        const back_buffer_view = renderer.core.swapChain().getCurrentTextureView();
+        const color_attachment = gpu.RenderPassColorAttachment{
+            .view = back_buffer_view,
+            .clear_value = std.mem.zeroes(gpu.Color),
+            .load_op = .clear,
+            .store_op = .store,
+        };
+        const command_encoder = renderer.core.device().createCommandEncoder(null);
+        const render_pass_info = gpu.RenderPassDescriptor.init(.{
+            .color_attachments = &.{color_attachment},
+        });
+        const pass = command_encoder.beginRenderPass(&render_pass_info);
+        pass.setPipeline(renderer.pipeline);
+        pass.setVertexBuffer(0, renderer.vertex_buffer, 0, @sizeOf(Vertex) * renderer.vertices.items.len);
+        pass.draw(@intCast(u32, renderer.vertices.items.len), 1, 0, 0);
+        pass.end();
+        pass.release();
+
+        var command = command_encoder.finish(null);
+        command_encoder.release();
+        renderer.queue.submit(&[_]*gpu.CommandBuffer{command});
+        command.release();
+        renderer.core.swapChain().present();
+        back_buffer_view.release();
+    }
+
+    pub fn drawFilledRectangle(renderer: *Renderer, x: f32, y: f32, width: f32, height: f32) !void {
+        const window_size = renderer.core.size();
+        const half_window_w = @intToFloat(f32, window_size.width) * 0.5;
+        const half_window_h = @intToFloat(f32, window_size.height) * 0.5;
+        const new_x = x / half_window_w - 1.0;
+        const new_y = 1.0 - y / half_window_h;
+        const new_width = width / half_window_w;
+        const new_height = height / half_window_h;
+        const color = [4]f32{1.0, 1.0, 0.0, 1.0};
+
+        try renderer.vertices.append(.{.pos = .{ new_x + new_width, new_y, 0.1, 1.0 }, .col = color});
+        try renderer.vertices.append(.{.pos = .{ new_x, new_y, 0.1, 1.0 }, .col = color});
+        try renderer.vertices.append(.{.pos = .{ new_x, new_y - new_height, 0.1, 1.0 }, .col = color});
+
+        try renderer.vertices.append(.{.pos = .{ new_x, new_y - new_height, 0.1, 1.0 }, .col = color});
+        try renderer.vertices.append(.{.pos = .{ new_x + new_width, new_y - new_height, 0.1, 1.0 }, .col = color});
+        try renderer.vertices.append(.{.pos = .{ new_x + new_width, new_y, 0.1, 1.0 }, .col = color});
+    }
+
+};
